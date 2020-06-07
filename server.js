68747da3c908893d4350644143db453606720e3a
caroldf07 <carolina.df@outlook.com>
2020-06-06 23:08:34 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const server = express();
+
+server.use(express.static(__dirname));
+
+server.get("/",(req,res) => {
+    res.render("./index.html")
+})
+
+server.listen(5000)
